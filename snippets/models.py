9a8477476e1f4a17cabe37195f84e7222b5414fb
Bhuwan pandeya <bpandeya70@gmail.com>
2018-09-17 12:59:20 +0545

    Initial commit

---
+++ snippets/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return self.title
---
+++ snippets/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Snippet
+from .serializers import SnippetSerializer
+from rest_framework import generics
+# Create your views here.
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
